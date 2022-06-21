46234323a6d2a5d7c6d26a4febf953ce5dd1bddf
Cristian-Ariel-Tolaba <cristianesesc1476@gmail.com>
2022-06-20 23:30:19 -0300

    Tp listo

--- app.js
+++ app.js
@@ -18,4 +18,7 @@ app.get("/belgrano", (req,res) => {res.sendFile(path.join(__dirname, "views","be
 
 app.listen(port, () => {console.log("Servidor corriendo en http://localhost:" + port)});
 
-app.use(express.static("public)"));
+app.use(express.static("public"));
+
+
+//Feliz dia del padre Profes//
